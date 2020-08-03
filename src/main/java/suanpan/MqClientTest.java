adf2b8e89dc555b15561f888ab71d28e254cc351
xuyuhan <792174229@qq.com>
2020-08-03 17:30:08 +0800

    test

---
+++ src/main/java/suanpan/MqClientTest.java
@@ -0,0 +1,28 @@
+package suanpan;
+
+import com.xuelang.mqstream.MqClient;
+import com.xuelang.mqstream.RedisStreamMqClient;
+import com.xuelang.mqstream.options.Message;
+
+/**
+ * @author changqing.xyh
+ * @date 2020/08/03 15:20
+ */
+
+public class MqClientTest {
+    public static void main(String[] args) {
+        Message message = Message.builder()
+                .queue("test_queue")
+                .keysAndValues(
+                        Message.prepareKeysAndValues("name", "xuelang", "city", "wuxi")
+                )
+                .build();
+
+        MqClient mqClient = new RedisStreamMqClient("127.0.0.1");
+        String messageId = mqClient.sendMessage(message);
+
+        System.out.println(messageId);
+    }
+
+
+}
---
+++ src/main/java/suanpan/TestDemo.java
@@ -0,0 +1,9 @@
+package suanpan;
+
+/**
+ * @author changqing.xyh
+ * @date 2020/08/03 17:29
+ */
+
+public class TestDemo {
+}
